cb0537b83c8228d9e938a0729b1c49e0d45d462b
齐哈 <Cherazade1412@protonmail.com>
2023-10-22 18:44:21 +0200

    mod

--- neurone_artificiel.py
+++ neurone_artificiel.py
@@ -8,8 +8,8 @@ y = y.reshape((y.shape[0], 1))
 print('dimension de X:', X.shape)
 print('dimension de y:', y.shape)
 
-plt.scatter(X[:,0], X[:,1], c=y, cmap='summer')
-plt.show
+#plt.scatter(X[:,0], X[:,1], c=y, cmap='summer')
+#plt.show
 
 def init(X):
     W = np.random.randn(X.shape[1], 1)
@@ -32,3 +32,35 @@ def log_loss(A, y):
     return (1/len(y)) * np.sum(-y * np.log(A) - (1-y) * np.log(1 - A))
 
 print(log_loss(A, y))
+
+def gradients(A, X, y):
+    dw = 1/len(y) * np.dot(X.T, A - y)
+    db = 1/len(y) * np.sum(A - y)
+    return(dw, db)
+
+dw, db = gradients(A, X, y)
+print(dw.shape)
+print(db.shape)
+
+def update(dw, db, W, b, learning_rate):
+    W = W - learning_rate * dw
+    b = b - learning_rate * db
+    return (W, b)
+
+def artificial_neuron(X, y, learning_rate = 0.1, n_iter = 100):
+    # initialize parameters
+    W, b = init(X)
+
+    loss = []
+
+    for i in range (n_iter):
+        A = model(X, W, b)
+        loss.append(log_loss(A, y))
+        dW, db = gradients(A, X, y)
+        W, b = update(dW, db, W, b, learning_rate)
+
+    plt.plot(loss)
+    plt.show()
+
+artificial_neuron(X, y)
+
