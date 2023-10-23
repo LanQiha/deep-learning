0927eb9cf7b6393402ff4a84d12bcb78af57884c
齐哈 <Cherazade1412@protonmail.com>
2023-10-23 13:18:32 +0200

    mod

--- artificial_neuron.py
+++ artificial_neuron.py
@@ -1,15 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.datasets import make_blobs
+from sklearn.metrics import accuracy_score
+from utilities import *
 
 X,y = make_blobs(n_samples=100, n_features=2, centers=2, random_state=0)
 y = y.reshape((y.shape[0], 1))
 
 print('dimension de X:', X.shape)
 print('dimension de y:', y.shape)
-
-#plt.scatter(X[:,0], X[:,1], c=y, cmap='summer')
-#plt.show
 
 def init(X):
     W = np.random.randn(X.shape[1], 1)
@@ -47,8 +46,12 @@ def update(dw, db, W, b, learning_rate):
     b = b - learning_rate * db
     return (W, b)
 
+def predict(X, W, b):
+    A = model(X, W, b)
+    print(A)
+    return A >= 0.5
+
 def artificial_neuron(X, y, learning_rate = 0.1, n_iter = 100):
-    # initialize parameters
     W, b = init(X)
 
     loss = []
@@ -59,8 +62,24 @@ def artificial_neuron(X, y, learning_rate = 0.1, n_iter = 100):
         dW, db = gradients(A, X, y)
         W, b = update(dW, db, W, b, learning_rate)
 
-    plt.plot(loss)
-    plt.show()
+    #y_pred = predict(X, W, b)
+    #print(accuracy_score(y, y_pred))
 
-artificial_neuron(X, y)
+    #plt.plot(loss)
+    #plt.show()
+
+    return(W, b)
+
+W, b = artificial_neuron(X, y)
+
+new_plant = np.array([2, 1])
+
+x0 = np.linspace(-1, 4, 100)
+x1 = (-W[0] * x0 - b)/W[1]
+
+print(predict(new_plant, W, b))
+plt.scatter(X[:,0], X[:,1], c=y, cmap='summer')
+plt.scatter(new_plant[0], new_plant[1], c='r')
+plt.plot(x0, x1, c='orange', lw=3)
+plt.show()
 
